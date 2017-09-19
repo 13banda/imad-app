b92f04cc3c00c91b393d8ec8a94a87dcce2eaaf3
13banda <31692610+13banda@users.noreply.github.com>
2017-09-19 17:10:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.get('/article-one',function(req,res){
 });
 
 app.get('/article-two',function(req,res){
-    res.send("this is article two is servered");
+    res.sendfile(path.join(__dirname,'ui','article-two.html'));
 });
 
 app.get('/article-three',function(req,res){
-    res.send("this is article three is servered");
+   res.sendfile(path.join(__dirname,'ui','article-three.html'));
 });
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));

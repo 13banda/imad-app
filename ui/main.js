73034f9a73d383e374efcc226f9696e37f03a831
13banda <31692610+13banda@users.noreply.github.com>
2017-09-20 12:21:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element=document.getElementById("main-text");
 element.innerHTML='new values';
 
 var img=document.getElementById('madi');
-ima.onclick=function(){
+img.onclick=function(){
     img.style.moveLeft='100px';
 }
 

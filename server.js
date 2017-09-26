1f8d9dca96d97035cc10d16f87a5238fe1e72a88
Sandeep singh <31692610+13banda@users.noreply.github.com>
2017-09-26 10:14:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,6 +84,14 @@ app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter+'');
 });
+
+var names=[];
+app.get('/submit-name',function(req,res){//url something /submit-name?name=xxxx;
+    var name=req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName',function(req,res){
     //this is the functionality of express framework
     // when we use colums then it is like a parameter 
@@ -111,12 +119,7 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-var names=[];
-app.get('/submit-name/:name',function(req,res){
-    var name=req.params.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
+
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 

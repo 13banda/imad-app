a0cac6652fb52f4b6f45c3894645375339f26cfc
Sandeep singh <31692610+13banda@users.noreply.github.com>
2017-10-04 17:00:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ function hash(input,salt){
 }
 app.get('/hash/:value',function(req,res){
    
-   var hashString =hash(req.params.value,"This-is the-salt-String");
+   var hashString =hash(req.params.value,"this-is-the-some-rendom-string");
    res.send(hashString);    
 });
 

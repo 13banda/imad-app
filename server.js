1ce1fdd6b8b9394f63613132c869fb935dd94a39
Sandeep singh <31692610+13banda@users.noreply.github.com>
2017-10-05 15:49:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -198,9 +198,11 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/display.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'display.css'));
 });
+
 app.get('/ui/display', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Display.html'));
 });
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });

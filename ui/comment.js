7d82a84ae7f9c2cec39bddf2e5255db14afe837e
Sandeep singh <31692610+13banda@users.noreply.github.com>
2017-10-09 20:17:53 +0530

    [imad-console] Updates ui/comment.js

--- ui/comment.js
+++ ui/comment.js
@@ -1,1 +1,24 @@
 
+// make a request to server
+var request=new XMLHttpRequest();
+var commentPanel=document.getElementById("comment_panel");
+request.onreadystatechange=function(){
+    if(request.readyState===XMLHttpRequest.DONE){
+        //take some action
+        if(request.status===200){
+            var commentsText=request.responseText;
+            var comments=JSON.parse(commentsText);
+            var commentBody="";
+            for(let i=0;i<comments.length;i++){
+                commentBody+="<h4></h4>"+comments.username+"<h5> at "+comments.timestemp+" </h5>"+comments.comment+"<hr>";
+            }
+            commentPanel.innerHTML=""+commentBody;
+        }else{
+            commentPanel.innerHTML="something went worng!";
+        }
+    }
+};
+// MAke the Request
+commentPanel.innerHTML="comments are loadings...";
+request.open('GET','http://wwaheguru9509088985.imad.hasura-app.io/comments?article_id=1',true);
+request.send(null);

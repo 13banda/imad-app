19dc92232409ca37cfd005fe2936b90649f242d8
Sandeep singh <31692610+13banda@users.noreply.github.com>
2017-10-15 13:16:09 +0530

    [imad-console] Updates ui/submitcomment.js

--- ui/submitcomment.js
+++ ui/submitcomment.js
@@ -1,5 +1,6 @@
 
 var Submit_btn=document.getElementById('submit_comment_btn');
+var commentText=document.getElementById('comment_Area').value;
 Submit_btn.onclick= function (){
     // make the request to the server and get the name list object
         var request=new XMLHttpRequest();
@@ -7,17 +8,17 @@ Submit_btn.onclick= function (){
                 if(request.readyState===XMLHttpRequest.DONE){
                     //take some action
                     if(request.status===200){
-                        // babnner set logout banner
+                              // babnner set logout banner
                                 var comments=request.responseText;
                                 comments=JSON.parse(comments);
-                                 console.log(comments);
-                                  console.log(comments[0].username);
                                 var commentBody="";
                                 for(let i=0;i<comments.length;i++){
                                    commentBody+="<p>"+comments[i].comment+"</p><h5>"+comments[i].username+" - "+comments[i].timestemp+"</h5><hr>";
                                 }
                                 var commentPanel=document.getElementById("comment_panel");
                                 commentPanel.innerHTML=""+commentBody;
+                                //set the text area to normal
+                                commentText.value='';
                     }
                     else{
                         alert('something went wrong!');
@@ -25,9 +26,7 @@ Submit_btn.onclick= function (){
                 }
             };
 
-                        // MAke the Request
-            var commentText=document.getElementById('comment_Area').value;
-            
-            request.open('GET','http://wwaheguru9509088985.imad.hasura-app.io/submit-comment?article_id=1&comment='+commentText,true);
-          request.send(null);
+// Make the Request
+request.open('GET','http://wwaheguru9509088985.imad.hasura-app.io/submit-comment?article_id=1&comment='+commentText,true);
+request.send(null);
 };

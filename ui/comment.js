9f29f8c2638912277751ba6e3e472ea142f18a88
Sandeep singh <31692610+13banda@users.noreply.github.com>
2017-10-20 12:50:43 +0530

    [imad-console] Updates ui/comment.js

--- ui/comment.js
+++ ui/comment.js
@@ -1,29 +1,31 @@
-
-// make a request to server
-var request=new XMLHttpRequest();
-var commentPanel=document.getElementById("comment_panel");
-request.onreadystatechange=function(){
-    if(request.readyState===XMLHttpRequest.DONE){
-        //take some action
-        if(request.status===200){
-            var comments=request.responseText;
-           
-            comments=JSON.parse(comments);
-             console.log(comments);
-              console.log(comments[0].username);
-            var commentBody="";
-            for(let i=0;i<comments.length;i++){
-               commentBody+="<p>"+comments[i].comment+"</p><h5>"+comments[i].username+" - "+Date(comments[i].timestemp).toDateString()+"</h5><hr>";
+var article_title=window.location.pathname.split('/')[2];
+function loadComments(){
+    // make a request to server
+    var request=new XMLHttpRequest();
+    var commentPanel=document.getElementById("comment_panel");
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            //take some action
+            if(request.status===200){
+                var comments=request.responseText;
+               
+                comments=JSON.parse(comments);
+                 console.log(comments);
+                  console.log(comments[0].username);
+                var commentBody="";
+                for(let i=0;i<comments.length;i++){
+                   commentBody+="<p>"+comments[i].comment+"</p><h5>"+comments[i].username+" - "+Date(comments[i].timestemp).toDateString()+"</h5><hr>";
+                }
+                commentPanel.innerHTML=""+commentBody;
+            }else{
+                commentPanel.innerHTML="something went worng!";
             }
-            commentPanel.innerHTML=""+commentBody;
-        }else{
-            commentPanel.innerHTML="something went worng!";
         }
-    }
-};
+    };
+}
 // MAke the Request
 commentPanel.innerHTML="comments are loadings...";
-request.open('GET','http://wwaheguru9509088985.imad.hasura-app.io/comments?article_id=1',true);
+request.open('GET','http://wwaheguru9509088985.imad.hasura-app.io/comments?article=article_title',true);
 request.send(null);
 
 var request_1=new XMLHttpRequest();

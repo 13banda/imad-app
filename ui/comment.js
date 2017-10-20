32d1d7100e0ee0ec5fff71be2661708cff38859f
Sandeep singh <31692610+13banda@users.noreply.github.com>
2017-10-20 14:23:01 +0530

    [imad-console] Updates ui/comment.js

--- ui/comment.js
+++ ui/comment.js
@@ -27,32 +27,63 @@ function loadComments(){
     request.send(null);
 }
 
-var request_1=new XMLHttpRequest();
-var newcommentPanel=document.getElementById("add_comment");
-request_1.onreadystatechange=function(){
-    if(request_1.readyState===XMLHttpRequest.DONE){
-        //take some action
-        if(request_1.status===200){
-            var commentPanel=request_1.responseText;
-            newcommentPanel.innerHTML=""+commentPanel;
-            
-            var jsElm = document.createElement("script");
+function loadNewCommentArea(){
+    var request=new XMLHttpRequest();
+    var newcommentPanel=document.getElementById("add_comment");
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            //take some action
+            if(request.status===200){
+                if(request.responseText!=='not login'){
+                var username=request.responseText;
+                newcommentPanel.innerHTML=`submit a comment 
+                                         <br><textarea id="comment_Area" placeholder="add a comment here..." ></textarea>
+                                         <br><input id="submit_comment_btn" type="submit" value="submit">`;
+                
+                            var Submit_btn=document.getElementById('submit_comment_btn');
+                            var commentText=document.getElementById('comment_Area');
+                            Submit_btn.onclick= function (){
+                                // make the request to the server and get the name list object
+                                    var request=new XMLHttpRequest();
+                                        request.onreadystatechange=function(){
+                                            if(request.readyState===XMLHttpRequest.DONE){
+                                                //take some action
+                                                if(request.status===200){
+                                                 commentText.value='';
+                                                 loadComments();
+                                                }
+                                                else{
+                                                    alert('something went wrong!');
+                                                }
+                                            }
+                            };
+                            request.open('GET','http://wwaheguru9509088985.imad.hasura-app.io/submit-comment?article='+article_title+'&comment='+commentText.value,true);
+                            request.send(null);
+                            };
+               }else{newcommentPanel.innerHTML="";}
+            }else{
+                newcommentPanel.innerHTML="";
+            }
+        }
+    };
+    // Make the Request
+    request.open('GET','http://wwaheguru9509088985.imad.hasura-app.io/check-login',true);
+    request.send(null);
+}
+
+loadNewCommentArea();
+loadComments();
+
+
+// extra practice
+let k=() =>{
+var jsElm = document.createElement("script");
             // set the type attribute
             jsElm.type = "application/javascript";
             // make the script element load file
             jsElm.src = '/ui/submitcomment.js';
             // finally insert the element to the body element in order to load the script
             newcommentPanel.appendChild(jsElm);
-        }else{
-            newcommentPanel.innerHTML="";
-        }
-    }
-};
-// MAke the Request
-request_1.open('GET','http://wwaheguru9509088985.imad.hasura-app.io/add-new-comment-panel',true);
-request_1.send(null);
+    };
 
-
-loadComments();
-
-
+            

bbf1d027c15ba94e85660bba30f6583d03efb686
Sandeep singh <31692610+13banda@users.noreply.github.com>
2017-10-24 14:26:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(session({
     secret: 'thisisthesecretrandomvalues',
     cookie: {
             secure: true,
-            httpOnly: true,
+            
             maxAge:1000 * 60 * 60 * 24 * 30
          }
 }));

c5e5d1b4fac2f4414fc3f804b458a496be16c86d
Sandeep singh <31692610+13banda@users.noreply.github.com>
2017-10-24 11:53:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var crypto = require('crypto');//for hashing the password
 var bodyParser= require('body-parser');// for extracting JSON from content body
 var session= require('express-session');// this the session lib.
 // helmet = require('tls');// see https://expressjs.com/en/advanced/best-practice-security.html
-var compression = require('compression');
+//var compression = require('compression');
 
 // db crandential
 var config ={
@@ -21,7 +21,7 @@ var config ={
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-app.use(compression());
+//app.use(compression());
 app.disable('x-powered-by');//atacke not find that app runing on express.js
 //var helmet = require('helmet');app.use(helmet());
 app.use(session({
